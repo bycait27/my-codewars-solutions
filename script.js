d58b91caca320db6078532a2810ae10964f02879
Caitlin Ash <caitlinba27@gmail.com>
2023-12-04 15:41:38 -0600

    Added first solution

---
+++ script.js
@@ -0,0 +1,8 @@
+// create a function that takes an integer as an argument and returns 'Even' for even numbers or 'Odd' for odd numbers
+function evenOrOdd(number) {
+    // if statement checking if the numbers are evenly divisible by 2
+    if (number % 2 == 0) {
+      return 'Even';
+    } else 
+      return 'Odd';
+  }
